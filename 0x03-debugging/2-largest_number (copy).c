4260942e14c76cb23d667a08e538ec2fe11f4474
Happy-32 <yahayaenmaariandrew@gmail.com>
2023-02-23 22:05:50 +0000

    Debugging

--- 0x03-debugging/2-largest_number (copy).c
+++ 0x03-debugging/2-largest_number (copy).c
@@ -10,17 +10,22 @@
 
 int largest_number(int a, int b, int c)
 {
+	int largest;
 
-	int largest = b;
-
-	if (a > b)
+	if (a > b && a > c)
 	{
 		largest = a;
+
 	}
-	else if (c > b)
+	else if (b > a && b > c)
+	{
+		largest = b;
+	}
+	else if (c > a && c > b)
 	{
 		largest = c;
 	}
+
 
 	return (largest);
 }
