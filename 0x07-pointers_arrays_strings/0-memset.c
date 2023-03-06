114c9bae3f2badca24fe7e5bae2117ed5ff0b166
Happy-32 <yahayaenmaariandrew@gmail.com>
2023-03-06 20:51:11 +0000

    More pointers

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+/**
+ * *_memset - fills the first @n bytes of the memory
+ * area pointed to by @s with the constant byte @b
+ * @b: the source
+ * @s: the destiation
+ * @n: Number of bytes of memory
+ * Return:pointer to the resulting string
+ */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	memset(s, b, n);
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,8 +1,17 @@
 #include "main.h"
 
+/**
+ * *_memcpy - copies @n bytes from memory
+ * area @src to memory area @dest
+ * @src: the source
+ * @dest: the destiation
+ * @n: number of bytes
+ * Return:pointer to the resulting string
+ */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	memcpy(dest,src,n);
-	putchar(*dest);
+	memcpy(dest, src, n);
+	_putchar(*dest);
 	return (0);
 }
