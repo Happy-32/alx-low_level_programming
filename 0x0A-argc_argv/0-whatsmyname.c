04e3be8eb51533a26525d8bf1c097ad99775919c
Happy-32 <yahayaenmaariandrew@gmail.com>
2023-03-12 11:48:24 +0000

    Command line arguments for programs

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,16 +9,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int count;
-
-	if (argc > 1)
-	{
-		for (count = 1; count < argc; count++)
-		{
-			printf("argv[%d] = %s\n", count, argv[count]);
-		}
-	}
-	printf("\"%s\".\n", argv[0]);
+	printf("%s\n", argv[argc*0]);
 
 	return (0);
 }
