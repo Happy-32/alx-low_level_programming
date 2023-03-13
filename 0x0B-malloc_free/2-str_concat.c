083e8758c0c5ca9748b761f60055598da9331264
Happy-32 <yahayaenmaariandrew@gmail.com>
2023-03-13 20:54:27 +0000

    Dynamic memory allocation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,30 +12,30 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *concat_string;
-	index index, concat_ind, len;
+	int index, concat_ind, len;
 
 	concat_ind = 0;
 	len = 0;
 
 	if (s1 == NULL)
-		s1 = " ";
-	
-	if (s2 = NULL)
-		s2 = " ";
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
 	
 	for (index = 0; s1[index] || s2[index]; index++)
 		len++;
 	
-	concat_string = malloc(sizeof(char) * len)
+	concat_string = malloc(sizeof(char) * len);
 
-	if (concat_str == NULL)
+	if (concat_string == NULL)
 		return (NULL);
 
 	for (index = 0; s1[index]; index++)
-		concat_str[concat_index++] = s1[index];
+		concat_string[concat_ind++] = s1[index];
 
 	for (index = 0; s2[index]; index++)
-		concat_str[concat_index++] = s2[index];
+		concat_string[concat_ind++] = s2[index];
 
-	return (concat_str);
+	return (concat_string);
 }
