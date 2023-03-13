b486480c9df4ae7f1e9cd14430e2d262198dc106
Happy-32 <yahayaenmaariandrew@gmail.com>
2023-03-13 20:05:59 +0000

    Dynamic memory allocation

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *create_array - creates an array
+ * @size: the size of the array
+ * @c: the content of each cell in array
+ *
+ * Return: array.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	array = (char *)malloc(size * sizeof(char));
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+
+	return (array);
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strdup - creates an array
+ * @str: the size of the array
+ *
+ * Return: array.
+ */
+char *_strdup(char *str)
+{
+	char *dupe;
+	int index, len;
+	len = 0;
+	if (str == NULL)
+		return (NULL);
+
+	dupe = malloc(sizeof(char) * (len + 1));
+
+	for (index = 0; str[index]; index++)
+		dupe[index] = str[index];
+
+
+	return (dupe);
+}
